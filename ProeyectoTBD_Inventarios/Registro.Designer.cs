19825ec81b0e0e6c4ec01ac46c2d27b4e3fca4e6
Isu-682 <padillamachayariana@gmail.com>
2025-12-17 00:00:01 -0600

    Form de Registro

---
+++ ProeyectoTBD_Inventarios/Registro.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ProeyectoTBD_Inventarios
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btRegistrarse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbCorreo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbContraseña = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbConfirmarCont = new System.Windows.Forms.TextBox();
+            this.btVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbUsuario
+            // 
+            this.txtbUsuario.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbUsuario.Location = new System.Drawing.Point(216, 73);
+            this.txtbUsuario.Name = "txtbUsuario";
+            this.txtbUsuario.Size = new System.Drawing.Size(196, 25);
+            this.txtbUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(20, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre  de  Usuario";
+            // 
+            // btRegistrarse
+            // 
+            this.btRegistrarse.BackColor = System.Drawing.Color.SlateGray;
+            this.btRegistrarse.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRegistrarse.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btRegistrarse.Location = new System.Drawing.Point(138, 316);
+            this.btRegistrarse.Name = "btRegistrarse";
+            this.btRegistrarse.Size = new System.Drawing.Size(110, 34);
+            this.btRegistrarse.TabIndex = 2;
+            this.btRegistrarse.Text = "Registrarse";
+            this.btRegistrarse.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(100, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CREAR CUENTA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(20, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo  Electronico";
+            // 
+            // txtbCorreo
+            // 
+            this.txtbCorreo.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbCorreo.Location = new System.Drawing.Point(216, 136);
+            this.txtbCorreo.Name = "txtbCorreo";
+            this.txtbCorreo.Size = new System.Drawing.Size(196, 25);
+            this.txtbCorreo.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(20, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtbContraseña
+            // 
+            this.txtbContraseña.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbContraseña.Location = new System.Drawing.Point(216, 194);
+            this.txtbContraseña.Name = "txtbContraseña";
+            this.txtbContraseña.Size = new System.Drawing.Size(196, 25);
+            this.txtbContraseña.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(20, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Confirmar  Contraseña";
+            // 
+            // txtbConfirmarCont
+            // 
+            this.txtbConfirmarCont.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbConfirmarCont.Location = new System.Drawing.Point(216, 257);
+            this.txtbConfirmarCont.Name = "txtbConfirmarCont";
+            this.txtbConfirmarCont.Size = new System.Drawing.Size(196, 25);
+            this.txtbConfirmarCont.TabIndex = 8;
+            // 
+            // btVolver
+            // 
+            this.btVolver.BackColor = System.Drawing.Color.SlateGray;
+            this.btVolver.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Italic);
+            this.btVolver.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btVolver.Location = new System.Drawing.Point(12, 339);
+            this.btVolver.Name = "btVolver";
+            this.btVolver.Size = new System.Drawing.Size(71, 31);
+            this.btVolver.TabIndex = 10;
+            this.btVolver.Text = "Volver";
+            this.btVolver.UseVisualStyleBackColor = false;
+            // 
+            // Registro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(444, 382);
+            this.Controls.Add(this.btVolver);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtbConfirmarCont);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbContraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbCorreo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btRegistrarse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbUsuario);
+            this.Name = "Registro";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btRegistrarse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbCorreo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbContraseña;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbConfirmarCont;
+        private System.Windows.Forms.Button btVolver;
+    }
+}
---
+++ ProeyectoTBD_Inventarios/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ProeyectoTBD_Inventarios
+{
+    public partial class Registro : Form
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+    }
+}
