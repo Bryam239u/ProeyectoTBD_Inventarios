a5ed1836a5ab179023bfc74c718eb6eb0f0391f8
Isu-682 <padillamachayariana@gmail.com>
2025-12-17 02:34:26 -0600

    diseño de Sesion

---
+++ ProeyectoTBD_Inventarios/Sesiones.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProeyectoTBD_Inventarios
+{
+    partial class Sesiones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSesion = new System.Windows.Forms.DataGridView();
+            this.btnVolver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSesion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSesion
+            // 
+            this.dgvSesion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSesion.Location = new System.Drawing.Point(13, 13);
+            this.dgvSesion.Name = "dgvSesion";
+            this.dgvSesion.Size = new System.Drawing.Size(620, 257);
+            this.dgvSesion.TabIndex = 0;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(12, 326);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 1;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // Sesiones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 361);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.dgvSesion);
+            this.Name = "Sesiones";
+            this.Text = "Sesiones";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSesion)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSesion;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}
