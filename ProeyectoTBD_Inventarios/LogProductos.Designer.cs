62f5e8403b7453e1d1a18cddbe1ac35b3707511a
Isu-682 <padillamachayariana@gmail.com>
2025-12-17 02:48:21 -0600

    form LogProductos

---
+++ ProeyectoTBD_Inventarios/LogProductos.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProeyectoTBD_Inventarios
+{
+    partial class LogProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.dgvLogProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(11, 325);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 3;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // dgvLogProductos
+            // 
+            this.dgvLogProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogProductos.Location = new System.Drawing.Point(12, 12);
+            this.dgvLogProductos.Name = "dgvLogProductos";
+            this.dgvLogProductos.Size = new System.Drawing.Size(620, 257);
+            this.dgvLogProductos.TabIndex = 2;
+            // 
+            // LogProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 359);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.dgvLogProductos);
+            this.Name = "LogProductos";
+            this.Text = "LogProductos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.DataGridView dgvLogProductos;
+    }
+}
