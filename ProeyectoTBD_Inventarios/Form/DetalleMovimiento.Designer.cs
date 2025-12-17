ef8f6a3be1f7ca31e4ba434425fe64311a8e99c3
Isu-682 <padillamachayariana@gmail.com>
2025-12-17 02:17:34 -0600

    Diseño y codigo de Detalle Movimiento

---
+++ ProeyectoTBD_Inventarios/Form/DetalleMovimiento.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ProeyectoTBD_Inventarios
+{
+    partial class DetalleMovimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvDetalles = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(22, 62);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbProducto.TabIndex = 0;
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(23, 156);
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(120, 20);
+            this.numCantidad.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cantidad";
+            // 
+            // dgvDetalles
+            // 
+            this.dgvDetalles.AllowUserToAddRows = false;
+            this.dgvDetalles.AllowUserToDeleteRows = false;
+            this.dgvDetalles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalles.Location = new System.Drawing.Point(200, 29);
+            this.dgvDetalles.Name = "dgvDetalles";
+            this.dgvDetalles.ReadOnly = true;
+            this.dgvDetalles.Size = new System.Drawing.Size(416, 240);
+            this.dgvDetalles.TabIndex = 4;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(33, 215);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(95, 30);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // DetalleMovimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 309);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvDetalles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numCantidad);
+            this.Controls.Add(this.cmbProducto);
+            this.Name = "DetalleMovimiento";
+            this.Text = "DetalleMovimiento";
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvDetalles;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
---
+++ ProeyectoTBD_Inventarios/Form/DetalleMovimiento.cs
@@ -0,0 +1,64 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Windows.Forms;
+
+namespace ProeyectoTBD_Inventarios
+{
+    public partial class DetalleMovimiento : Form
+    {
+        public DetalleMovimiento()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (cmbProducto.SelectedValue == null)
+            {
+                MessageBox.Show("Debe seleccionar un producto.");
+                return;
+            }
+
+            if (numCantidad.Value <= 0)
+            {
+                MessageBox.Show("La cantidad debe ser mayor a cero.");
+                return;
+            }
+
+            string connectionString = "User Id=TU_USUARIO;Password=TU_PASSWORD;Data Source=TU_DB_ORACLE;";
+
+            string sql = @"INSERT INTO DetalleMovimiento (IdMovimiento, IdProducto, Cantidad) 
+                   VALUES (:idMov, :idProd, :cant)";
+
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (OracleCommand cmd = new OracleCommand(sql, conn))
+                    {
+
+                        cmd.Parameters.Add("idMov", OracleDbType.Int32).Value = 1;
+                        cmd.Parameters.Add("idProd", OracleDbType.Int32).Value = cmbProducto.SelectedValue;
+                        cmd.Parameters.Add("cant", OracleDbType.Int32).Value = numCantidad.Value;
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Renglón guardado correctamente.");
+
+                        numCantidad.Value = 1;
+                        cmbProducto.SelectedIndex = -1;
+                    }
+                }
+                catch (OracleException ex)
+                {
+                    MessageBox.Show("Error de Base de Datos: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error general: " + ex.Message);
+                }
+
+            }
+        }
+    }
+}
